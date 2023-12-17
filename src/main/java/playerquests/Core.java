664ef965885c0625111976f173d385271a349125
sammypanda <38725335+sammypanda@users.noreply.github.com>
2023-12-17 22:26:15 +1000

    test broadcastMessage: onEnable -> onLoad
    line executed before the server has a chance to load the plugin so was not showing up

--- src/main/java/playerquests/Core.java
+++ src/main/java/playerquests/Core.java
@@ -1,11 +1,16 @@
 package playerquests;
 
 import org.bukkit.plugin.java.JavaPlugin; // essential for initialising the plugin
+import org.bukkit.Bukkit; // required for getServer/Bukkit
 
 public class Core extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getServer().broadcastMessage("Working");
     }
+
+    @Override
+    public void onLoad() {
+	    getServer().broadcastMessage("Working");
+	}
 }
