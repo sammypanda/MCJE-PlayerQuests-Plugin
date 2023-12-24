ee348a2a254aa292d33c681dcc70c31fe244b527
sammypanda <38725335+sammypanda@users.noreply.github.com>
2023-12-24 19:16:40 +1000

    added titles to GUI

--- src/main/java/playerquests/gui/GUI.java
+++ src/main/java/playerquests/gui/GUI.java
@@ -1,8 +1,9 @@
 package playerquests.gui;
 
-import org.bukkit.Bukkit;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.entity.HumanEntity;
+import org.bukkit.Bukkit; // used to refer to base spigot/bukkit methods
+import org.bukkit.inventory.Inventory; // used to manage the GUI
+import org.bukkit.inventory.InventoryView; // used to manage the GUI screen
+import org.bukkit.entity.HumanEntity; // the subject(s) the GUI shows for
 
 /**
  * The interface for creating and opening a GUI.
@@ -11,8 +12,10 @@ import org.bukkit.entity.HumanEntity;
  */
 public class GUI {
 
-    private HumanEntity humanEntity;
-    private Inventory inventory;
+    private HumanEntity humanEntity; // the subject the screen will open on
+    private Inventory inventory; // the empty inventory used as a GUI screen
+    private InventoryView inventoryView; // the screen itself when open
+    private String title; // the title of the screen (InventoryView)
 
     {
         this.inventory = Bukkit.createInventory(this.humanEntity, 0);
@@ -32,10 +35,30 @@ public class GUI {
      */
     public void open() {
         // Display the inventory
-        this.humanEntity.openInventory(this.inventory);
+        this.inventoryView = this.humanEntity.openInventory(this.inventory);
+
+        // Set the title (done here as it requires InventoryView, or to construct a new GUI)
+        this.inventoryView.setTitle(this.title);
 
         // Testing
         System.out.println("Reached via GUI");
+    }   
+
+    /**
+     * Sets the title of the screen.
+     * Applies in GUI {@link #open()}, only for the top InventoryView. 
+     * @param title The label of the GUI screen.
+     */
+    public void setTitle(String title) {
+        this.title = title; // class variable to set title when InventoryView becomes accessible
+    }
+
+    /**
+     * Provides the string value for the title of the screen.
+     * @return title The label of the GUI screen.
+     */
+    public String getTitle() {
+        return this.title;
     }
 
     /**
