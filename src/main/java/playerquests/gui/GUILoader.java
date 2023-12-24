1117b8e459ae243827309b533809be3c8f133783
sammypanda <38725335+sammypanda@users.noreply.github.com>
2023-12-24 19:22:26 +1000

    added JSON parse for GUI template (with title field)

--- src/main/java/playerquests/gui/GUILoader.java
+++ src/main/java/playerquests/gui/GUILoader.java
@@ -1,6 +1,16 @@
 package playerquests.gui;
 
-import org.bukkit.entity.HumanEntity;
+import java.io.IOException; // used if the JSON GUI template file can't be loaded
+import java.nio.file.Files; // locating the JSON GUI template file
+import java.nio.file.Path; // to keep and use a file path
+import java.nio.file.Paths; // to get paths
+import java.util.Optional; // tolerates if a json field is or is not set
+
+import org.bukkit.entity.HumanEntity; // the human entity (usually player) which will see the GUI
+
+import com.fasterxml.jackson.core.JsonProcessingException; // catch-all erroring for the JSON files
+import com.fasterxml.jackson.databind.JsonNode; // the java-friendly object for holding the JSON info
+import com.fasterxml.jackson.databind.ObjectMapper; // reads the JSON
 
 /**
  * Parses the GUI screen templates as a GUI.
@@ -8,8 +18,10 @@ import org.bukkit.entity.HumanEntity;
  */
 public class GUILoader {
 
+    private ObjectMapper jsonObjectMapper = new ObjectMapper();
     private GUI gui;
     private HumanEntity humanEntity;
+    private Path path;
 
     /**
      * Constructs a new GUILoader ready to parse the JSON templates.
@@ -30,13 +42,69 @@ public class GUILoader {
     }
     
     /**
-     * Translating a GUI screen template from JSON into a {@link GUI}.
+     * Translating a GUI screen template from JSON file into a {@link GUI}.
      * <p>
      * JSON template layout in Specification section of README.
-     * @param    template The name of the template json file excluding .json
+     * @param    templateFile The name of the template json file excluding .json
      * @return   An instance of {@link GUI}
      */
-    public GUI load(String template) {
+    public GUI load(String templateFile) {
+        // Init variable where the JSON string will be put
+        String templateString = new String();
+
+        // Define the path where screens can be found and
+        // Attach the templateFile parameter to the path
+        this.path = Paths.get("src","main","resources","gui","screens",templateFile);
+
+        // Pull out the json file as a string
+        try {
+            templateString = Files.readString(this.path);
+        } catch (IOException e) { // On an I/O failure such as the file not being found
+            System.err.println(this.path + " is not a valid path");
+        }
+
+        // Process the template into a real GUI screen
+        parse(templateString);
+
         return this.gui;
+    }
+
+    /**
+     * Translating a GUI screen template from a JSON string into a {@link GUI}.
+     * <p>
+     * JSON template layout in Specification section of README.
+     * @param    templateString
+     * @return   An instance of {@link GUI}
+     */
+    public GUI parse(String templateString) {
+        // Init variable where the GUITemplate object will be put
+        JsonNode template;
+
+        // Convert the JSON string into a GUITemplate object
+        // This makes it easier to pull values out of the JSON 
+        try {
+            // readValue(String content, Class<T> valueType)
+            // Method to deserialize JSON content from given JSON content String.
+            template = this.jsonObjectMapper.readTree(templateString);
+
+            // to flexibly set the values from keys to the GUI screen 
+            parseTitle(template);
+
+        } catch (JsonProcessingException e) { // Encapsulates all JSON processing errors that could occur
+            System.err.println("The template passed into parse(here) failed to map, JSON: " + templateString);
+        }
+
+        return this.gui;
+    }
+
+    /**
+     * Take the 'title' template key and set it as the title for the gui.
+     */
+    private void parseTitle(JsonNode node) {
+        String title = Optional.ofNullable(node.get("title")) // get title field if it exists
+            .map(JsonNode::asText) // if exists get it as text (String)
+            .orElse(""); // if not set it as an empty string
+
+        this.gui.setTitle(title);
     }
 }
