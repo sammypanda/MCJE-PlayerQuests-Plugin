f8ee8bc3f141efd85a18537fc490b686249eea3b
sammypanda <38725335+sammypanda@users.noreply.github.com>
2024-01-02 19:48:47 +1000

    removed unused imports from Core

--- src/main/java/playerquests/Core.java
+++ src/main/java/playerquests/Core.java
@@ -1,9 +1,7 @@
 package playerquests;
 
 import org.bukkit.plugin.java.JavaPlugin; // essential for initialising the plugin
-import playerquests.gui.GUILoader; // parses template JSON files into GUI types
-import playerquests.chat.command.Commandplayerquest;
-import playerquests.gui.GUI; // used to create and control GUIs
+import playerquests.chat.command.Commandplayerquest; // used to show the plugin as a GUI
 
 /**
  * Entry point for the plugin.
