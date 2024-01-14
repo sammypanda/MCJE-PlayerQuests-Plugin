72f8896314c10033ba5046e645197172bffa798f
sammypanda <38725335+sammypanda@users.noreply.github.com>
2024-01-15 00:17:38 +1000

    added comments to CloseScreen class

--- src/main/java/playerquests/gui/function/CloseScreen.java
+++ src/main/java/playerquests/gui/function/CloseScreen.java
@@ -1,7 +1,13 @@
 package playerquests.gui.function;
 
+/**
+ * Fully closes the GUI window.
+ */
 public class CloseScreen extends GUIFunction {
 
+    /**
+     * Calls the close() method in the GUI class.
+     */
     @Override
     public void execute() {
         this.parentGui.close();
