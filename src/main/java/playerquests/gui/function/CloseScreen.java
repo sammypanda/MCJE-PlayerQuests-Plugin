ff5e6d53f98dd997b4f071f6d806ae6cdf393f4a
utsu-pinku <104600733+utsu-pinku@users.noreply.github.com>
2024-01-14 09:59:14 +0000

    Started creation of an exit within the GUI

---
+++ src/main/java/playerquests/gui/function/CloseScreen.java
@@ -0,0 +1,9 @@
+import org.bukkit.Bukkit;
+
+public class CloseScreen extends GUIFunction {
+
+    @Override
+    public void execute() {
+        Bukkit.broadcastMessage("Sammy is beautiful :D");
+    }
+}
