a89670a6a809fd2782edfe67517c912c72cfaa0a
sammypanda <38725335+sammypanda@users.noreply.github.com>
2024-01-18 18:01:57 +1000

    added empty constructor to ChatClient

--- src/main/java/playerquests/client/chat/ChatClient.java
+++ src/main/java/playerquests/client/chat/ChatClient.java
@@ -4,5 +4,12 @@ package playerquests.client.chat;
  * Controlling the plugin via the chatbox.
  */
 public class ChatClient {
-    // TODO: implement ChatClient
+
+    /**
+     * Constructs a new client for controlling plugin via the chatbox.
+     */
+    public ChatClient() {
+        // TODO: implement ChatClient
+    }
+
 }
