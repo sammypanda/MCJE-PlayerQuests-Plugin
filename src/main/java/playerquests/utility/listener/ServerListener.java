b9a8f37912d6f28008bef6913e67886c8257eb7b
sammypanda <38725335+sammypanda@users.noreply.github.com>
2024-02-23 16:47:15 +1000

    register players as questers on reload
    the return could be removed to make it also do the same for on startup but if that was necessary it would be a bug either in our onJoin or in spigots join event.

--- src/main/java/playerquests/utility/listener/ServerListener.java
+++ src/main/java/playerquests/utility/listener/ServerListener.java
@@ -4,26 +4,34 @@ import org.bukkit.Bukkit; // bukkit API
 import org.bukkit.event.EventHandler; // indicate that a method is wanting to handle an event
 import org.bukkit.event.Listener; // registering listening to Bukkit in-game events
 import org.bukkit.event.player.PlayerJoinEvent; // called when players have loaded into the game
+import org.bukkit.event.server.ServerLoadEvent;
 
 import playerquests.Core; // accessing plugin singeltons
 import playerquests.client.quest.QuestClient; // represents a quest player/quest tracking
 import playerquests.utility.singleton.QuestRegistry;
 
 /**
- * Listens out for all player-related events to inform 
+ * Listens out for all server-related events to inform 
  * where needed.
  */
-public class PlayerListener implements Listener {
+public class ServerListener implements Listener {
 
-    public PlayerListener() {
+    public ServerListener() {
         Bukkit.getPluginManager().registerEvents(this, Core.getPlugin());
     }
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
-        // create a quest client and add it to registry.
+    public void onReload(ServerLoadEvent event) {
+        if (!event.getType().equals(ServerLoadEvent.LoadType.RELOAD)) {
+            return;
+        }
+
+        // for every user, create a quest client and add it to registry.
         // this enables a player to interact with and track quests.
-        QuestClient quester = new QuestClient(event.getPlayer());
-        QuestRegistry.getInstance().addQuester(quester);
+        Bukkit.getServer().getOnlinePlayers().stream()
+            .forEach(player -> {
+                QuestClient quester = new QuestClient(player);
+                QuestRegistry.getInstance().addQuester(quester);
+            });
     }
 }
