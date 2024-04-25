e927ac7cdcb99c6a2cde5ec77e38ba1337920f01
sammypanda <38725335+sammypanda@users.noreply.github.com>
2024-04-25 17:49:46 +1000

    added empty myquest/theirquest GUIs

---
+++ src/main/java/playerquests/builder/gui/dynamic/Dynamicmyquest.java
@@ -0,0 +1,19 @@
+package playerquests.builder.gui.dynamic;
+
+import playerquests.client.ClientDirector;
+
+public class Dynamicmyquest extends GUIDynamic {
+
+    public Dynamicmyquest(ClientDirector director, String previousScreen) {
+        super(director, previousScreen);
+    }
+
+    @Override
+    protected void setUp_custom() {
+    }
+
+    @Override
+    protected void execute_custom() {
+    }
+    
+}
---
+++ src/main/java/playerquests/builder/gui/dynamic/Dynamictheirquest.java
@@ -0,0 +1,19 @@
+package playerquests.builder.gui.dynamic;
+
+import playerquests.client.ClientDirector;
+
+public class Dynamictheirquest extends GUIDynamic {
+
+    public Dynamictheirquest(ClientDirector director, String previousScreen) {
+        super(director, previousScreen);
+    }
+
+    @Override
+    protected void setUp_custom() {
+    }
+
+    @Override
+    protected void execute_custom() {
+    }
+    
+}
