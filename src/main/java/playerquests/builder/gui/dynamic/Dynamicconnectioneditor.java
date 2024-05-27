de0b411959468e6a43d96582133eae1f500ee162
sammypanda <38725335+sammypanda@users.noreply.github.com>
2024-05-28 00:07:54 +1000

    added unimplemented connectioneditor buttons

--- src/main/java/playerquests/builder/gui/dynamic/Dynamicconnectioneditor.java
+++ src/main/java/playerquests/builder/gui/dynamic/Dynamicconnectioneditor.java
@@ -1,5 +1,6 @@
 package playerquests.builder.gui.dynamic;
 
+import playerquests.builder.gui.component.GUISlot;
 import playerquests.builder.quest.data.ConnectionsData;
 import playerquests.client.ClientDirector;
 
@@ -22,6 +23,39 @@ public class Dynamicconnectioneditor extends GUIDynamic {
     @Override
     protected void execute_custom() {
         this.director.getPlayer().sendMessage(this.connections.toString());
+
+        String prev = this.connections.getPrev();
+        String curr = this.connections.getCurr();
+        String next = this.connections.getNext();
+
+        this.gui.getFrame().setTitle("Sequence Editor");
+
+        new GUISlot(gui, 1)
+            .setItem("PISTON")
+            .setLabel(
+                String.format("%s %s", 
+                    prev != null ? "Change Previous Step" : "Set Previous Step",
+                    prev != null ? "("+prev+")" : ""
+                )
+            );
+
+        new GUISlot(gui, 2)
+            .setItem("PISTON")
+            .setLabel(
+                String.format("%s %s", 
+                    curr != null ? "Change Current Step" : "Set Current Step",
+                    curr != null ? "("+curr+")" : ""
+                )
+            );
+
+        new GUISlot(gui, 3)
+            .setItem("PISTON")
+            .setLabel(
+                String.format("%s %s", 
+                    next != null ? "Change Next Step" : "Set Next Step",
+                    next != null ? "("+next+")" : ""
+                )
+            );
     }
     
 }
