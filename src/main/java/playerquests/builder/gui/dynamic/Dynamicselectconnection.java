c19a278851cc7e4ff7d97613b5bd7d8786916e62
sammypanda <38725335+sammypanda@users.noreply.github.com>
2024-05-29 15:43:19 +1000

    implement stages recursion in 'select connection'

--- src/main/java/playerquests/builder/gui/dynamic/Dynamicselectconnection.java
+++ src/main/java/playerquests/builder/gui/dynamic/Dynamicselectconnection.java
@@ -2,14 +2,21 @@ package playerquests.builder.gui.dynamic;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.IntStream;
 
 import playerquests.builder.gui.component.GUIFrame;
 import playerquests.builder.gui.component.GUISlot;
 import playerquests.builder.gui.function.UpdateScreen;
+import playerquests.builder.quest.QuestBuilder;
 import playerquests.client.ClientDirector;
 
 public class Dynamicselectconnection extends GUIDynamic {
+
+    /**
+     * The quest we are editing.
+     */
+    QuestBuilder questBuilder;
 
     public Dynamicselectconnection(ClientDirector director, String previousScreen) {
         super(director, previousScreen);
@@ -17,6 +24,7 @@ public class Dynamicselectconnection extends GUIDynamic {
 
     @Override
     protected void setUp_custom() {
+        this.questBuilder = (QuestBuilder) director.getCurrentInstance(QuestBuilder.class);
     }
 
     @Override
@@ -26,7 +34,15 @@ public class Dynamicselectconnection extends GUIDynamic {
         frame.setTitle("Select Connection");
         frame.setSize(27);
 
+        int rangeOffset = 1; // the amount to subtract from the slot number, to get an index from 0
+        List<String> stages = questBuilder.getStages();
         IntStream.range(1, 19).forEach((int slot) -> {
+            int i = slot - rangeOffset;
+
+            if (stages.size() < i + 1) {
+                return;
+            }
+
             new GUISlot(gui, slot)
                 .setLabel("[A Stage]")
                 .setItem("CHEST");
