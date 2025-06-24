ca54ac73fcd536580feffffa45b4058e8d221eff
sammypanda <38725335+sammypanda@users.noreply.github.com>
2025-06-24 16:33:40 +1000

    migrate SelectMaterial to Paper

--- src/main/java/playerquests/builder/gui/function/SelectMaterial.java
+++ src/main/java/playerquests/builder/gui/function/SelectMaterial.java
@@ -4,7 +4,6 @@ import java.util.List; // used for creating deniedBlocks list
 import java.util.stream.Collectors; // transforming stream to data type
 
 import org.bukkit.Bukkit; // getting the plugin manager
-import org.bukkit.ChatColor;
 import org.bukkit.Material; // the resulting material (block)
 import org.bukkit.block.Block; // spigot block type
 import org.bukkit.entity.Player;
@@ -12,10 +11,14 @@ import org.bukkit.event.EventHandler; // registering methods as event handlers
 import org.bukkit.event.HandlerList; // unregistering event handlers
 import org.bukkit.event.Listener; // listening to in-game events
 import org.bukkit.event.inventory.InventoryClickEvent; // for detecting block selections in listener
-import org.bukkit.event.player.AsyncPlayerChatEvent; // handling request to exit
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerInteractEvent; // for detecting block hits in listener
 
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import playerquests.Core; // accessing singletons
 import playerquests.builder.gui.function.data.SelectMethod; // defining which methods to select something
 import playerquests.client.ClientDirector; // controls the plugin
@@ -132,7 +135,7 @@ public class SelectMaterial extends GUIFunction {
          * @param event the {@code AsyncPlayerChatEvent} triggered when a player sends a chat message
          */
         @EventHandler
-        private void onChat(AsyncPlayerChatEvent event) {
+        private void onChat(AsyncChatEvent event) {
             // if the event is coming from a different player
             if (this.player != event.getPlayer()) {
                 return; // do not capture other players events
@@ -141,7 +144,7 @@ public class SelectMaterial extends GUIFunction {
             event.setCancelled(true);
 
             Bukkit.getScheduler().runTask(Core.getPlugin(), () -> { // run on next tick
-                String message = event.getMessage();
+                String message = PlainTextComponentSerializer.plainText().serialize(event.message());
 
                 // if wanting to exit (or trying to do another command)
                 if (ChatUtils.isExitKeyword(message)) {
@@ -334,27 +337,26 @@ public class SelectMaterial extends GUIFunction {
         ChatUtils.clearChat(this.player);
 
         if (this.cancelled) {
-            this.player.sendMessage(
-                ChatColor.GRAY + "" + ChatColor.ITALIC + "exited" + ChatColor.RESET
-            );
+            ChatUtils.message(
+                Component.text("exited").color(NamedTextColor.GRAY).decorate(TextDecoration.ITALIC)
+            ).player(player).send();
             this.exit();
             return;
         }
 
         if (this.result == null) {
-            this.player.sendMessage(
-                ChatColor.UNDERLINE + this.prompt + ChatColor.RESET
-            );
-            ChatUtils.clearChat(this.player, 1);
-            this.player.sendMessage(
-                ChatColor.RED + "or type " + ChatColor.GRAY + "exit" + ChatColor.RESET
-            );
+            ChatUtils.message(
+                Component.text(this.prompt).decorate(TextDecoration.UNDERLINED)
+                .appendNewline().appendNewline()
+                .append(Component.text("or type ").color(NamedTextColor.RED))
+                .append(Component.text("exit").color(NamedTextColor.GRAY))
+            ).player(player).send();
             return;
         }
 
-        this.player.sendMessage(
-            ChatColor.GRAY + "" + ChatColor.ITALIC + "Selected: " + result.toString()
-        );
+        ChatUtils.message(
+            Component.text("Selected: " + result.toString()).decorate(TextDecoration.ITALIC).color(NamedTextColor.GRAY)
+        ).player(player).send();
 
         this.exit(); // finish
     }
