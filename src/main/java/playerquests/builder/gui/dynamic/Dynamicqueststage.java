53bdd7e7e80235facac1fca73fa604485f7bd32b
sammypanda <38725335+sammypanda@users.noreply.github.com>
2025-07-20 21:31:35 +1000

    reduce cognitive complexity of quest stage GUI body

--- src/main/java/playerquests/builder/gui/dynamic/Dynamicqueststage.java
+++ src/main/java/playerquests/builder/gui/dynamic/Dynamicqueststage.java
@@ -77,33 +77,151 @@ public class Dynamicqueststage extends GUIDynamic {
         this.gui.getFrame().setSize(27);
 
         // the back button
-        GUISlot exitButton = new GUISlot(this.gui, 19);
-        exitButton.setLabel("Back");
-        exitButton.setItem(Material.OAK_DOOR);
-        exitButton.addFunction(new UpdateScreen( // set function as 'UpdateScreen'
-            Arrays.asList("queststages"), // set the previous screen
-            director // set the client director
-        ));
+        this.createBackButton();
 
         // setting stage label
-        new GUISlot(gui, 1)
-            .setItem(Material.OAK_SIGN)
-            .setLabel(String.format("%s stage label",
-                this.questStage.hasLabel() ? "Change" : "Set"
-            ))
-            .onClick(() -> {
-                new ChatPrompt(
-                    Arrays.asList("Type a label to help you remember the stage", "none"),
-                    director
-                ).onFinish((func) -> {
-                    ChatPrompt function = (ChatPrompt) func;
-                    String response = function.getResponse();
-                    this.questStage.setLabel(response);
-                    this.refresh();
-                }).execute();
-            });
+        this.createStageLabelButton();
 
         // setting startpoint actions
+        this.createStartPointButton();
+
+        // left side dividers
+        this.createDividerSlots();
+
+        // produce slots listing current actions
+        if (!confirmActionKeys) {
+            for (var index : IntStream.range(0, actionKeys.size()).toArray()) {
+                if (this.createActionButton(index)) {
+                    break; // exit early
+                }
+            }
+
+            // set actionKeys as confirmed
+            this.confirmActionKeys = true;
+        }
+
+        // add 'delete stage' button (with confirm)
+        this.createDeleteButton();
+
+        // add 'new action' button
+        this.createNewActionButton();
+    }
+
+    private void createNewActionButton() {
+        GUISlot newActionButton = new GUISlot(this.gui, this.gui.getFrame().getSize());
+        if (this.questStage.getActions().size() >= 18) {
+            newActionButton.setLabel("No More Action Slots");
+            newActionButton.setItem(Material.BARRIER);
+            return;
+        }
+        
+
+        newActionButton.setLabel("Add Action");
+        newActionButton.setItem(Material.LIME_DYE);
+        newActionButton.onClick(() -> {
+            // create the new action
+            questStage.addAction(new NoneAction(questStage));
+
+            // refresh UI
+            this.confirmActionKeys = false; // set actionKeys to be looped through again
+            this.gui.clearSlots();
+            this.execute(); // re-run to see new action in list
+        });
+    }
+
+    private void createDeleteButton() {
+        if (!this.questBuilder.removeStage(this.questStage, true)) { // if cannot delete the stage
+            new GUISlot(this.gui, this.gui.getFrame().getSize() - 1)
+                .setItem(Material.GRAY_DYE)
+                .setLabel("Cannot Delete")
+                .setDescription(List.of("This stage is connected to other stages and actions."));
+            return;
+        }
+
+        if (!this.confirmDelete) { // if delete hasn't been confirmed
+            new GUISlot(this.gui, this.gui.getFrame().getSize() - 1)
+                .setItem(Material.RED_DYE)
+                .setLabel("Delete Stage")
+                .onClick(() -> {
+                    this.confirmDelete = true;
+                    this.execute();
+                });
+            return;
+        }
+
+        new GUISlot(this.gui, this.gui.getFrame().getSize() - 1)
+            .setItem(Material.RED_WOOL)
+            .setLabel("Delete")
+            .onClick(() -> {
+                if (this.questBuilder.removeStage(this.questStage)) { // if quest was removed
+                    new UpdateScreen(
+                        Arrays.asList(previousScreen),
+                        this.director
+                    ).execute();
+
+                    // update the quest
+                    QuestRegistry.getInstance().submit(this.questBuilder.build());
+                }
+
+                // update the quest
+                QuestRegistry.getInstance().submit(this.questBuilder.build());
+            });
+    }
+
+    private boolean createActionButton(int index) {
+        QuestAction action = actionKeys.get(index);
+        Integer nextEmptySlot = this.gui.getEmptySlot();
+        GUISlot actionSlot = new GUISlot(this.gui, nextEmptySlot);
+        String typeString = String.format("Type: %s", action.getName());
+        List<String> description;
+
+        // check if this action is a start point 
+        boolean isStartPoint = this.questStage.getStartPoints().stream()
+            .anyMatch(p -> 
+                p.getStage().equals(this.questStage.getID()) &&
+                p.getActions().contains(action.getID())
+            );
+
+        // set description
+        if (isStartPoint) {
+            description = List.of(typeString, "Is an entry point");
+        } else {
+            description = List.of(typeString);
+        }
+
+        actionSlot
+            .setLabel(String.format("%s", action.getLabel()))
+            .setDescription(description)
+            .setItem(action.getActionStateItem(isStartPoint));
+
+        actionSlot.onClick(() -> {
+            if (!this.gui.getFrame().getMode().equals(GUIMode.CLICK)) {
+                return;
+            }
+
+            // set the action as the current action to modify
+            this.director.setCurrentInstance(action, QuestAction.class);
+
+            // go to action editor screen
+            actionSlot.addFunction(new UpdateScreen(
+                Arrays.asList("actioneditor"),
+                director
+            )).execute(this.director.getPlayer());
+        });
+
+        return false; // continue the loop
+    }
+
+    private void createDividerSlots() {
+        new GUISlot(this.gui, 2)
+            .setItem(Material.BLACK_STAINED_GLASS_PANE);
+        new GUISlot(this.gui, 11)
+            .setItem(Material.BLACK_STAINED_GLASS_PANE);
+        new GUISlot(this.gui, 20)
+            .setItem(Material.BLACK_STAINED_GLASS_PANE);
+    }
+
+    private void createStartPointButton() {
         List<StagePath> startPoints = this.questStage.getStartPoints();
         new GUISlot(gui, 10)
             .setItem(Material.PISTON)
@@ -122,119 +240,34 @@ public class Dynamicqueststage extends GUIDynamic {
                     })
                     .execute();
             });
+    }
 
-        // left side dividers
-        new GUISlot(this.gui, 2)
-            .setItem(Material.BLACK_STAINED_GLASS_PANE);
-        new GUISlot(this.gui, 11)
-            .setItem(Material.BLACK_STAINED_GLASS_PANE);
-        new GUISlot(this.gui, 20)
-            .setItem(Material.BLACK_STAINED_GLASS_PANE);
-
-        // produce slots listing current actions
-        if (!confirmActionKeys) {
-            IntStream.range(0, actionKeys.size()).anyMatch(index -> {
-
-                QuestAction action = actionKeys.get(index);
-                Integer nextEmptySlot = this.gui.getEmptySlot();
-                GUISlot actionSlot = new GUISlot(this.gui, nextEmptySlot);
-                String typeString = String.format("Type: %s", action.getName());
-                List<String> description;
-
-                // check if this action is a start point 
-                boolean isStartPoint = this.questStage.getStartPoints().stream()
-                    .anyMatch(p -> 
-                        p.getStage().equals(this.questStage.getID()) &&
-                        p.getActions().contains(action.getID())
-                    );
-
-                // set description
-                if (isStartPoint) {
-                    description = List.of(typeString, "Is an entry point");
-                } else {
-                    description = List.of(typeString);
-                }
-
-                actionSlot
-                    .setLabel(String.format("%s", action.getLabel()))
-                    .setDescription(description)
-                    .setItem(action.getActionStateItem(isStartPoint));
-
-                actionSlot.onClick(() -> {
-                    if (!this.gui.getFrame().getMode().equals(GUIMode.CLICK)) {
-                        return;
-                    }
-
-                    // set the action as the current action to modify
-                    this.director.setCurrentInstance(action, QuestAction.class);
-
-                    // go to action editor screen
-                    actionSlot.addFunction(new UpdateScreen(
-                        Arrays.asList("actioneditor"),
-                        director
-                    )).execute(this.director.getPlayer());
-                });
-
-                return false; // continue the loop
+    private void createStageLabelButton() {
+        new GUISlot(gui, 1)
+            .setItem(Material.OAK_SIGN)
+            .setLabel(String.format("%s stage label",
+                this.questStage.hasLabel() ? "Change" : "Set"
+            ))
+            .onClick(() -> {
+                new ChatPrompt(
+                    Arrays.asList("Type a label to help you remember the stage", "none"),
+                    director
+                ).onFinish((func) -> {
+                    ChatPrompt function = (ChatPrompt) func;
+                    String response = function.getResponse();
+                    this.questStage.setLabel(response);
+                    this.refresh();
+                }).execute();
             });
+    }
 
-            // set actionKeys as confirmed
-            this.confirmActionKeys = true;
-        }
-
-        // add 'delete stage' button (with confirm)
-        if (!this.confirmDelete) { // if delete hasn't been confirmed
-            new GUISlot(this.gui, this.gui.getFrame().getSize() - 1)
-                .setItem(Material.RED_DYE)
-                .setLabel("Delete Stage")
-                .onClick(() -> {
-                    this.confirmDelete = true;
-                    this.execute();
-                });
-        } else {
-            new GUISlot(this.gui, this.gui.getFrame().getSize() - 1)
-                .setItem(Material.RED_WOOL)
-                .setLabel("Delete")
-                .onClick(() -> {
-                    if (this.questBuilder.removeStage(this.questStage)) { // if quest was removed
-                        new UpdateScreen(
-                            Arrays.asList(previousScreen),
-                            this.director
-                        ).execute();
-
-                        // update the quest
-                        QuestRegistry.getInstance().submit(this.questBuilder.build());
-                    }
-
-                    // update the quest
-                    QuestRegistry.getInstance().submit(this.questBuilder.build());
-                });
-        }
-
-        if (!this.questBuilder.removeStage(this.questStage, true)) { // if cannot delete the stage
-            new GUISlot(this.gui, this.gui.getFrame().getSize() - 1)
-                .setItem(Material.GRAY_DYE)
-                .setLabel("Cannot Delete")
-                .setDescription(List.of("This stage is connected to other stages and actions."));
-        }
-
-        // add 'new action' button
-        GUISlot newActionButton = new GUISlot(this.gui, this.gui.getFrame().getSize());
-        if (this.questStage.getActions().size() < 19) {
-            newActionButton.setLabel("Add Action");
-            newActionButton.setItem(Material.LIME_DYE);
-            newActionButton.onClick(() -> {
-                // create the new action
-                questStage.addAction(new NoneAction(questStage));
-
-                // refresh UI
-                this.confirmActionKeys = false; // set actionKeys to be looped through again
-                this.gui.clearSlots();
-                this.execute(); // re-run to see new action in list
-            });
-        } else {
-            newActionButton.setLabel("No More Action Slots");
-            newActionButton.setItem(Material.BARRIER);
-        }
+    private void createBackButton() {
+        GUISlot exitButton = new GUISlot(this.gui, 19);
+        exitButton.setLabel("Back");
+        exitButton.setItem(Material.OAK_DOOR);
+        exitButton.addFunction(new UpdateScreen( // set function as 'UpdateScreen'
+            Arrays.asList("queststages"), // set the previous screen
+            director // set the client director
+        ));
     }
 }
