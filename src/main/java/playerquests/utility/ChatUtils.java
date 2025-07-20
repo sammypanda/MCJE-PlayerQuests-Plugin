f9e23767747a17023c0898dd5c28a2b6c3555c69
sammypanda <38725335+sammypanda@users.noreply.github.com>
2025-07-20 21:47:51 +1000

    remove unnecessary boxed boolean

--- src/main/java/playerquests/utility/ChatUtils.java
+++ src/main/java/playerquests/utility/ChatUtils.java
@@ -386,7 +386,7 @@ public class ChatUtils {
      * @param keyword the string that might be an exit keyword
      * @return whether the keyword is or isn't indicating an exit
      */
-    public static Boolean isExitKeyword(String keyword) {
+    public static boolean isExitKeyword(String keyword) {
         return getExitKeywords().contains(keyword.toUpperCase());
     }
 
