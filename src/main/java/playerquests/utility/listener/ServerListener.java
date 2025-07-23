93e4808d30d1c3b46b03af58aaa522d95e17a415
sammypanda <38725335+sammypanda@users.noreply.github.com>
2025-07-23 11:26:59 +1000

    fix typo

--- src/main/java/playerquests/utility/listener/ServerListener.java
+++ src/main/java/playerquests/utility/listener/ServerListener.java
@@ -177,7 +177,7 @@ public class ServerListener implements Listener {
     private String getQuestName(Path path) {
         String[] questNameParts = path.toString()
             .replace(".json", "")
-            .split("/ " + Core.getQuestsPath());
+            .split("/" + Core.getQuestsPath());
 
         if (questNameParts.length < 1) {
             return null;
